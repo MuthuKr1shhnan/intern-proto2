c0602095395b02667666205fc323089a31882be0
MuthuKr1shhnan <2000muthukrishnan@gmail.com>
2025-07-14 19:25:03 +0530

    fixing cors issue

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+    proxy: {
+      '/api/pdf/merge': {
+        target: 'https://pdfbuddy-10101721923.development.catalystappsail.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
