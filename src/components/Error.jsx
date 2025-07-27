ddb0a8309fe7c8f6b29dbf74fb72903542a21151
MuthuKr1shhnan <2000muthukrishnan@gmail.com>
2025-07-27 14:26:12 +0530

    minor changes

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -3,9 +3,10 @@ import defaultErrorImage from "../assets/error.svg";
 function Error({ code = "Error", message = "Something went wrong. Please try again later.", image = defaultErrorImage }) {
   return (
     <div className='flex flex-col items-center justify-center h-[90vh] text-center px-4'>
-      <h1 className='text-6xl md:text-8xl font-extrabold text-gray-800'>{code}</h1>
+      
 
-      <img src={image} alt='Error Illustration' className='w-full max-w-md relative' />
+      <img src={image} alt='Error Illustration' className='w-full max-w-md ' />
+      <h1 className='text-6xl md:text-8xl font-extrabold text-gray-800 mb-2'>{code}</h1>
 
       <p className='text-base md:text-lg text-gray-600 max-w-xl'>
         {message}
